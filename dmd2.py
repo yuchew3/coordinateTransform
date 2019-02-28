d85accc35f64ee6ee74eacb7ddac5f178bedb9bb
Yuchen Wang <yuchew3@cs.uw.edu>
2019-02-28 09:55:52 -0800

    for large data

--- dmd2.py
+++ dmd2.py
@@ -5,26 +5,25 @@ from skimage import io
 from pydmd import DMD
 
 def main():
-    vid = io.imread('short_vid.tif') 
+    # when on cylon to test on full vid fdir = '../data/vid.tif'
+    vid = io.imread('../data/vid.tif') 
     flatten = np.transpose([np.asarray(x).flatten() for x in vid])
     X1 = flatten[:,:-1]
     X2 = flatten[:,1:]
 
+    print('start dmd')
     dmd = DMD(svd_rank=10)
     dmd.fit(flatten.T)
+    print('finished fitting data')
 
     # for eig in dmd.eigs:
     #     print('Eigenvalue {}: distance from unit circle {}'.format(eig, np.abs(eig.imag**2+eig.real**2 - 1)))
     # dmd.plot_eigs(show_axes=True, show_unit_circle=True)
 
-    other_recon = np.load('reconstructed.npy')
-    print(other_recon.shape)
     diff = dmd.reconstructed_data.T - flatten
-    print(vid.shape)
-    diff = diff.reshape(180, 240, 400)
-    print(np.amax(diff))
-    print(np.amax(other_recon - flatten))
-
+    diff = diff.reshape(180, 240, 40000)
+    np.save('../data/dmd/reconstructed', dmd.reconstructed_data.T.reshape((180,240,40000)))
+    np.save('../data/dmd/difference', diff)
 
 
 # use the original video for test
