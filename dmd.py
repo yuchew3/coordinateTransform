5d584625b9f405294e7df7db1cd5cad110fbfee9
Yuchen Wang <yuchew3@cs.uw.edu>
2019-03-01 01:15:58 -0800

    compared self dmd and lib dmd, difference is acceptable

--- dmd.py
+++ dmd.py
@@ -12,27 +12,27 @@ def dmd(X1, X2, r, dt):
     # U, S, V = np.linalg.svd(X1, full_matrices=False)
     print('start power iteration')
     startt = time.time()
-    S, U, V = power_svd.svd(X1, r)
-    pirnt('get a result in ' + str(time.time() - startt) + 's')
+    U, s, V = power_svd.svd(X1, r)
     V = V.conj().T
+    print('get a result in ' + str(time.time() - startt) + 's')
     # rank = min(r, U.shape[1])
 
     # U_r = U[:, :rank]
     # V_r = V[:, :rank]
     # s = S[:rank]
 
-    Atilde = U_r.T.conj().dot(X2).dot(V_r) * np.reciprocal(s)
+    Atilde = U.T.conj().dot(X2).dot(V) * np.reciprocal(s)
 
     eigenvalues_low, eigenvectors_low = np.linalg.eig(Atilde)
 
     eigenvalues_high = eigenvalues_low
-    eigenvectors_high = X2.dot(V_r) * np.reciprocal(s).dot(eigenvectors_low)
+    eigenvectors_high = (X2.dot(V) * np.reciprocal(s)).dot(eigenvectors_low)
 
     b = np.linalg.lstsq(eigenvectors_high, X1.T[0], rcond=False)[0]
 
     omega = old_div(np.log(eigenvalues_high), dt)
     ######
-    timesteps = np.arange(0, 39999 + dt, dt)
+    timesteps = np.arange(0, 399 + dt, dt)
     temp = np.exp(np.multiply(*np.meshgrid(omega, timesteps)))
     time_dynamics = (temp * b).T
     Xdmd = eigenvectors_high.dot(time_dynamics)
@@ -41,7 +41,7 @@ def dmd(X1, X2, r, dt):
 
 
 def main():
-    vid = io.imread('../data/vid.tif') 
+    vid = io.imread('short_vid.tif') 
     flatten = np.transpose([np.asarray(x).flatten() for x in vid])
     X1 = flatten[:,:-1]
     X2 = flatten[:,1:]
