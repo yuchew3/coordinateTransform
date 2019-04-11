ca6ccaae9eac3a2b725d522e16cb0126aeb8584a
Yuchen Wang <yuchew3@cs.uw.edu>
2019-04-11 14:24:44 -0700

    delete n_iter=200,300

--- svd_test.py
+++ svd_test.py
@@ -22,7 +22,7 @@ def svd_tolerance():
     U, s, V = randomized_svd(matrix, 
                               n_components=500)
     n_oversamples = [50, 100, 150, 200] # from 5 - 20
-    n_iters = [40, 70, 100, 200, 300] # from 4, 7, 
+    n_iters = [40, 70, 100] # from 4, 7, 
 
     norms = []
     for n in n_oversamples:
