e1d2ae5e839374b8be97a6cfaf237d90cd418723
Yuchen Wang <yuchew3@cs.uw.edu>
2019-04-16 15:39:33 -0700

    save trunk data

--- process_labels.py
+++ process_labels.py
@@ -6,9 +6,16 @@ def trunk_data_by_states(vid, labels):
     state_1 = np.where(labels==1)
     state_2 = np.where(labels==2)
     state_3 = np.where(labels==3)
-    print(state_1.shape)
-    print(state_2.shape)
-    print(state_3.shape)
+    print(state_1[0].shape)
+    print(state_2[0].shape)
+    print(state_3[0].shape)
+    sleep = vid[:][state_1[0]]
+    wake_1 = vid[:][state_2[0]]
+    wake_2 = vid[:][state_3[0]]
+    np.save('../data/sleep',sleep)
+    np.save('../data/wake_1',wake_1)
+    np.save('../data/wake_2',wake_2)
+
 
 
 def load_labels():
