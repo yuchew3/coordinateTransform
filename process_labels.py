0f10c24eca295871ac8077c34951c0bf2a212e6f
Yuchen Wang <yuchew3@cs.uw.edu>
2019-04-16 15:04:29 -0700

    typo

--- process_labels.py
+++ process_labels.py
@@ -2,7 +2,7 @@ import scipy.io
 import numpy as np
 
 def load_labels():
-    mat = io.loadmat('../data/frameStates.mat')
+    mat = scipy.io.loadmat('../data/frameStates.mat')
     mat = np.array(mat['b'])
     return mat[0][:-1]
 if __name__ == '__main__':
