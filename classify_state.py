c5d5388d689a42ae3a6a8c05f2cc84c21e8a78c9
Yuchen Wang <yuchew3@cs.uw.edu>
2019-04-23 14:09:44 -0700

    inconsistent size

--- classify_state.py
+++ classify_state.py
@@ -8,7 +8,7 @@ from sklearn.svm import SVC
 def main():
     vid = ca_data_utils.load_v_matrix()
     labels = ca_data_utils.load_labels()
-    X_train, X_test, y_train, y_test = train_test_split(vid, labels, test_size=0.25) # random state?
+    X_train, X_test, y_train, y_test = train_test_split(vid.T, labels, test_size=0.25) # random state?
     clf = SVC(kernel="linear", C=0.025)
     clf.fit(X_train, y_train)
     score = clf.score(X_test, y_test)
