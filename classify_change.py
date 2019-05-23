303397b7b7348c9a04b33e21f1f698308fafc04b
Yuchen Wang <yuchew3@cs.uw.edu>
2019-05-23 15:16:51 -0700

    MLP

--- classify_change.py
+++ classify_change.py
@@ -1,6 +1,18 @@
 import numpy as np
 from sklearn.svm import SVC
 from sklearn.neural_network import MLPClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.datasets import make_moons, make_circles, make_classification
+from sklearn.neural_network import MLPClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.gaussian_process import GaussianProcessClassifier
+from sklearn.gaussian_process.kernels import RBF
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
 import ca_data_utils
 
 def load_data():
@@ -26,7 +38,25 @@ if __name__ == '__main__':
     print(len(x))
     X = np.array(X)[x]
     labels = labels[x]
-    clf = MLPClassifier()
-    clf.fit(X[:cut], labels[:cut])
-    score = clf.score(X[cut:], labels[cut:])
-    print(score)
+
+    classifiers = [
+        KNeighborsClassifier(3),
+        SVC(kernel="linear", C=0.025),
+        SVC(gamma=2, C=1),
+        GaussianProcessClassifier(1.0 * RBF(1.0)),
+        DecisionTreeClassifier(max_depth=5),
+        RandomForestClassifier(max_depth=5, n_estimators=10, max_features=1),
+        MLPClassifier(alpha=1, max_iter=1000),
+        AdaBoostClassifier(),
+        GaussianNB(),
+        QuadraticDiscriminantAnalysis()]
+    
+    names = ["Nearest Neighbors", "Linear SVM", "RBF SVM", "Gaussian Process",
+         "Decision Tree", "Random Forest", "Neural Net", "AdaBoost",
+         "Naive Bayes", "QDA"]
+
+    for name, clf in zip(names, classifiers):
+        print('starting ', name)
+        clf.fit(X[:cut, labels[:cut]])
+        score = clf.score(X[cut:], labels[cut:])
+        print('---> test accuracy is ', str(score))
