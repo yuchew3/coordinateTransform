84b20cca7380cdd8920b6f00631aa5cfb880558e
Yuchen Wang <yuchew3@cs.uw.edu>
2019-05-23 14:03:35 -0700

    train aon change

---
+++ classify_change.py
@@ -0,0 +1,19 @@
+import numpy as np
+from sklearn.svm import SVC
+import ca_data_utils
+
+def load_data():
+    X = ca_data_utils.load_v_matrix().T[8:39992]
+    labels = ca_data_utils.load_labels()[8:39992]
+    labels[labels==3] = 2
+    labels = [labels[k+1] - labels[k] for k in range(len(labels)-1)]
+    X = [X[k+1] - X[k] for k in range(len(X) - 1)]
+    labels[labels != 0] = 1
+    return X, labels
+
+if __main__ == '__name__':
+    X, labels = load_data()
+    clf = SVC(C=0.1, gamma=0.01)
+    clf.fit(X[:30000], labels[:30000])
+    score = clf.score(X[30000:], labels[30000:])
+    print(score)
