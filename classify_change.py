283e83fecefc5b3cbde6a688c5e81c321255f60d
Yuchen Wang <yuchew3@cs.uw.edu>
2019-05-23 14:27:10 -0700

    wrong label

--- classify_change.py
+++ classify_change.py
@@ -6,8 +6,6 @@ def load_data():
     X = ca_data_utils.load_v_matrix().T[9:39992]
     labels = ca_data_utils.load_labels()[9:39992]
     labels[labels==3] = 2
-    print(np.where(labels == 1))
-    print(len(np.where(labels == 2)))
     labels = [labels[k+1] - labels[k] for k in range(len(labels)-1)]
     X = [X[k+1] - X[k] for k in range(len(X) - 1)]
     labels[labels != 0] = 1
@@ -18,7 +16,7 @@ if __name__ == '__main__':
     clf = SVC(C=0.1, gamma=0.01)
     clf.fit(X[:30000], labels[:30000])
     score = clf.score(X[30000:], labels[30000:])
-    a = np.where(labels==0)
+    a = np.where(labels==1)
     print(len(a[0]))
     print(len(labels))
     print(score)
