634c8c9c776e6dbf612e239c1d202e4879818de4
Yuchen Wang <yuchew3@cs.uw.edu>
2019-05-23 14:24:20 -0700

    ?

--- classify_change.py
+++ classify_change.py
@@ -5,9 +5,7 @@ import ca_data_utils
 def load_data():
     X = ca_data_utils.load_v_matrix().T[9:39992]
     labels = ca_data_utils.load_labels()[9:39992]
-    print(labels)
     labels[labels==3] = 2
-    print(labels)
     print(np.where(labels == 1))
     print(len(np.where(labels == 2)))
     labels = [labels[k+1] - labels[k] for k in range(len(labels)-1)]
@@ -20,7 +18,7 @@ if __name__ == '__main__':
     clf = SVC(C=0.1, gamma=0.01)
     clf.fit(X[:30000], labels[:30000])
     score = clf.score(X[30000:], labels[30000:])
-    a,b = np.where(labels==0)
-    print(len(a))
+    a = np.where(labels==0)
+    print(len(a[0]))
     print(len(labels))
     print(score)
