529e50349c72b78faba522b1d654b96e0e9d4090
Yuchen Wang <yuchew3@cs.uw.edu>
2019-05-28 16:44:48 -0700

    new autocorr matrix

--- autocorrelation.py
+++ autocorrelation.py
@@ -11,14 +11,25 @@ def load_data(step):
 
 if __name__ == "__main__":
     X, labels = load_data(2)
-    # X = X[:,0]
-    lags = 10
+    X = X[:,::4]
+    print(X.shape)
+    length = 10
+    lags = 5
     # dataset = np.fromfunction(lambda i, j: pd.Series(X[i]).autocorr(lag=j+1), (len(X), lags))
-    dataset = np.zeros((X.shape[1], lags))
-    for i in range(X.shape[1]):
-        series = pd.Series(X[:,i])
-        for j in range(lags):
-            corr = series.autocorr(lag=j+1)
-            dataset[i, j] = corr
-    np.save('../data/autocorr', dataset)
+    # for i in range(X.shape[0]-length):
+    #     series = pd.Series(X[:,i])
+    #     for j in range(lags):
+    #         corr = series.autocorr(lag=j+1)
+    #         dataset[i, j] = corr
+
+    dataset = np.zeros((X.shape[0]-length+1, X.shape[1]*lags))
+    for i in range(dataset.shape[0]):
+        x = X[i:i+length]
+        for p in range(X.shape[1]):
+            series = pd.Series(x[:,p])
+            for j in range(lags):
+                corr = series.autocorr(lag=j+1)
+                dataset[i, p*lags+j] = corr
+
+    np.save('../data/autocorr_2', dataset)
     print(dataset.shape)
