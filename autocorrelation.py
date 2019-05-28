40e5ee5070e436655acb923818e13575df5dd7f2
Yuchen Wang <yuchew3@cs.uw.edu>
2019-05-28 15:13:54 -0700

    autocorrelation

---
+++ autocorrelation.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+import ca_data_utils
+
+def load_data(step):
+    v = ca_data_utils.load_v_matrix().T[9:39992:step]
+    labels = ca_data_utils.load_labels()[9:39992:step]
+    return v, labels
+    
+
+
+if __name__ == "__main__":
+    X, labels = load_data(2)
+    X = X[:,0]
+    print(X.shape)
