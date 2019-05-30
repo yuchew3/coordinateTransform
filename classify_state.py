fdda43a2a3068859f06fa2713d061a545ac863ca
Yuchen Wang <yuchew3@cs.uw.edu>
2019-05-30 16:43:49 -0700

    tune l1 reg

--- classify_state.py
+++ classify_state.py
@@ -102,10 +102,10 @@ def kernel_pca():
     plt.tight_layout()
     plt.savefig('../data/clf_results/kpca')
 
-def xgboost(booster):
+def xgboost(reg_alpha):
     X = ca_data_utils.load_v_matrix().T[8:39992:5]
     labels = ca_data_utils.load_labels()[8:39992:5]
-    clf = xgb.XGBClassifier(booster=booster)
+    clf = xgb.XGBClassifier(reg_alpha=reg_alpha, learning_rate=0.1)
     total_len = len(labels)
     cut = int(3 * total_len / 4)
     clf.fit(X[:cut], labels[:cut])
@@ -114,6 +114,6 @@ def xgboost(booster):
 
 if __name__ == '__main__':
     # kernel_pca()
-    for i in ['gbtree', 'gblinear', 'dart']:
-        print('for boost = ', i)
+    for i in np.logspace(-3, 3, 7):
+        print('for L1 regularization = ', i)
         xgboost(i)
